82f6c6f33c219f1277850f358cfe9d00567ca673
SHERRYWANNA <243654677@163.com>
2018-06-01 18:01:07 +0800

    add scroll function

--- resource/js/layernative.js
+++ resource/js/layernative.js
@@ -9,7 +9,72 @@ function Layernative(item){
         tt,
         content,
         container,
-        btnarea;
+        btnarea,
+        isShow = {
+            num: 0,
+            addNum: function() {
+                this.num++;
+            },
+            reduceNum: function(limit) {
+                var _limit = limit ? limit : 0;
+                this.num--;
+                if (this.num < _limit) {
+                    this.num = _limit;
+                }
+            }
+        },
+        preventMove = {
+            scroll: false,
+            status: false,
+            startY: 0,
+            init: function(box, content) {
+                this.box = box;
+                this.content = content;
+                this.addEvent();
+            },
+            fun: function(e) {
+                var _event = e || event,
+                    _this = preventMove;
+
+                if (_this.scroll && _this.status) {
+                    var _py = e.targetTouches[0].pageY,
+                        _ch = _this.content.clientHeight,
+                        _sh = _this.content.scrollHeight,
+                        _st = _this.content.scrollTop;
+
+                    if (_st === 0 && _py > _this.startY) {
+                        _event.preventDefault();
+                        return;
+                    } else if (_ch + _st >= _sh && _py < _this.startY) {
+                        _event.preventDefault();
+                        return;
+                    }
+                } else {
+                    _event.preventDefault();
+                    return;
+                }
+
+            },
+            addEvent: function() {
+                addEvent(this.content, 'touchstart', function(e) {
+                    var _event = e || event;
+                    preventMove.status = true;
+                    preventMove.startY = _event.targetTouches[0].pageY;
+                });
+                addEvent(this.content, 'touchend', function() {
+                    preventMove.status = false;
+                });
+                addEvent(this.box, 'touchmove', this.fun);
+            },
+            isAddScroll: function() {
+                if (this.content.scrollHeight > this.content.clientHeight) {
+                    this.scroll = true;
+                } else {
+                    this.scroll = false;
+                }
+            }
+        };
+
     function render() {
         alertBox = document.createElement('div');
         alertBox.className = 'layer';
@@ -23,15 +88,12 @@ function Layernative(item){
         alertBox.innerHTML += _html;
         _body.appendChild(alertBox);
 
-        cover = query('.layer_cover', alertBox)[0];
-        addEvent(alertBox, 'touchmove', function(e) {
-            var _event = e || event;
-            _event.preventDefault();
-        });
         tt = query('.layer_tt', alertBox)[0];
         content = query('.layer_content', alertBox)[0];
         container = query('.layer_container', alertBox)[0];
         btnarea = query('.layer_btnarea', alertBox)[0];
+        cover = query('.layer_cover', alertBox)[0];
+        preventMove.init(alertBox, content);
     }
 
     function addEffect(effect, time) {
@@ -41,7 +103,7 @@ function Layernative(item){
                     if (effect === 'fade') {
                         $(this).fadeIn(time, callback);
                     } else {
-                        $(this).show();
+                        $(this).show(time, callback);
                     }
                     return this;
                 },
@@ -49,7 +111,7 @@ function Layernative(item){
                     if (effect === 'fade') {
                         $(this).fadeOut(time, callback);
                     } else {
-                        $(this).hide();
+                        $(this).hide(time, callback);
                     }
                     return this;
                 }
@@ -81,6 +143,26 @@ function Layernative(item){
         }
     }
 
+    function removeEvent(el, event, fun) {
+        if (el.attachEvent) {
+            el.detachEvent("on" + event, fun);
+        } else if (el.addEventListener) {
+            el.removeEventListener(event, fun, false);
+        }
+    }
+
+    function findParent(dom, parent) {
+        var _parent = dom.parentNode;
+        if (_parent != document.body && _parent != document.documentElement) {
+            if (_parent != parent) {
+                return findParent(_parent, parent);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
     if (!_$) {
         window.$ = function(el) {
             var obj = {
@@ -91,8 +173,12 @@ function Layernative(item){
                         _status = 'block';
                     }
                     this.css('display', _status);
-                    if (callback && time) {
-                        setTimeout(callback, time);
+                    if (callback) {
+                        if (time) {
+                            setTimeout(callback, time);
+                        } else {
+                            callback();
+                        }
                     }
                     return this;
                 },
@@ -149,13 +235,25 @@ function Layernative(item){
         // 当只有一个按钮时的按钮的class
         singleBtnClass: 'layer_btn-red',
         init: function(item) {
+            var _this = this;
+
             if (item) {
                 for (var i in item) {
                     this[i] = item[i];
                 }
             }
+            if (this.effect === 'show') {
+                this.time = 0;
+            }
             addEffect(this.effect, this.time);
             render();
+
+            if (this.coverHidden) {
+                addEvent(cover, 'click', function() {
+                    isShow.num = 1;
+                    _this.hide();
+                });
+            }
         },
         alert: function(title, word, btn, style) {
             var _this = this;
@@ -171,7 +269,7 @@ function Layernative(item){
 
             var _btnLen = btn.length;
             for ( var i = 0; i < _btnLen; i++ ) {
-                var _word = btn[i].word ? btn[i].word : 'ok~',
+                var _word = btn[i].word ? btn[i].word : '我知道了',
                     _btn = document.createElement('div'),
                     _callback = btn[i].callback;
 
@@ -184,12 +282,8 @@ function Layernative(item){
             }
 
             _this.show(style);
-            
-            if (_this.coverHidden) {
-                addEvent(cover, 'click', function() {
-                    _this.hide();
-                });
-            }
+
+            preventMove.isAddScroll();
            
             function addBtnFun($btn, callback, callbackHidden) {
                 addEvent($btn, 'click', function() {
@@ -197,6 +291,8 @@ function Layernative(item){
                         callback();
                         if ( callbackHidden ) {
                             _this.hide();
+                        } else {
+                            isShow.reduceNum(1);
                         }
                     } else {
                         _this.hide();
@@ -207,16 +303,20 @@ function Layernative(item){
         },
         show: function(style) {
             this.addStyle(style);
+            isShow.addNum();
 
             $(container).layerEffectIn(this.time);
             $(cover).layerEffectIn(this.time);
         },
         hide: function() {
-            $(container).LayerEffectOut(this.time, function(){
-                content.innerHTML = '';
-                tt.innerHTML = '';
-            });
-            $(cover).LayerEffectOut(this.time);
+            isShow.reduceNum();
+            if (!isShow.num) {
+               $(container).LayerEffectOut(this.time, function(){
+                   content.innerHTML = '';
+                   tt.innerHTML = '';
+               });
+               $(cover).LayerEffectOut(this.time); 
+            }
         },
         clearStyle: function() {
             var _style = $(container).attr('style'),
@@ -242,6 +342,11 @@ function Layernative(item){
             } else if (typeof style === 'string') {
                 alertBox.className = 'layer ' + style;
             }
+        },
+        destroy: function() {
+            $(container).LayerEffectOut(this.time, function(){
+                alertBox.parentNode.removeChild(alertBox);
+            });
         }
     };
     obj.init(item);
